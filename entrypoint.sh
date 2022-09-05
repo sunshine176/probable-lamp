0d2a4193e395cd5bcef5c5d83532829596925085
sunshine176 <112854969+sunshine176@users.noreply.github.com>
2022-09-05 08:24:07 +0000

    1

---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ -z "${Password}" ]]; then
+  Password="876c419e-036e-4812-9cae-91dcd7803ce5"
+fi
+ENCRYPT="chacha20-ietf-poly1305"
+QR_Path="/2a2a6445-d1d1-4eba-9dc1-5c52269479b9"
+
+#V2Ray Configuration
+V2_Path="/b1c47846-9251-46f8-b563-eae448fb90bb"
+V_VER=`wget -qO- "https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
+mkdir /wwwroot
+mkdir /v2raybin
+cd /v2raybin
+V2RAY_URL="https://github.com/shadowsocks/v2ray-plugin/releases/download/${V_VER}/v2ray-plugin-linux-amd64-${V_VER}.tar.gz"
+wget -q --no-check-certificate ${V2RAY_URL}
+tar -zxvf v2ray-plugin-linux-amd64-$V_VER.tar.gz
+rm -rf v2ray-plugin-linux-amd64-$V_VER.tar.gz
+mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
+rm -rf /v2raybin
+
+if [ ! -d /etc/shadowsocks-libev ]; then  
+  mkdir /etc/shadowsocks-libev
+fi
+
+# TODO: bug when PASSWORD contain '/'
+sed -e "/^#/d"\
+    -e "s/\${PASSWORD}/${Password}/g"\
+    -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
+    -e "s|\${V2_Path}|${V2_Path}|g"\
+    /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
+echo /etc/shadowsocks-libev/config.json
+cat /etc/shadowsocks-libev/config.json
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${V2_Path}|${V2_Path}|g"\
+    -e "s|\${QR_Path}|${QR_Path}|g"\
+    -e "$s"\
+    /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
+
+if [ "${Domain}" = "no" ]; then
+  echo "septemberburns"
+else
+  plugin=$(echo -n "v2ray;path=${V2_Path};host=${Domain};tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
+  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443?plugin=${plugin}" 
+  echo "${ss}" | tr -d '\n' > /wwwroot/index.html
+  echo -n "${ss}" | qrencode -s 6 -o /wwwroot/vpn.png
+fi
+
+ss-server -c /etc/shadowsocks-libev/config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
